bbbf9b25e3e98521442cbed7219338b0f3c9bb29
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-05-15 16:18:40 +0200

    add-simplelightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,39 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const imgItems = document.querySelector('.gallery');
+
+const imgMarkup = createImgItemsMarkup(galleryItems);
+
+imgItems.addEventListener('click', onOpenMaxImgModal);
+
+imgItems.insertAdjacentHTML('beforeend', imgMarkup);
+
+function createImgItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+function onOpenMaxImgModal(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  let lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250,
+    showCounter: false,
+    alertError: false,
+    captionsData: 'alt',
+  });
+}
