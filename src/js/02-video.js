5fdcadfa507ae403fb82153017d3e0854dbddbd2
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-05-23 13:46:49 +0100

    do_player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
 
+// const timePlay = localStorage.getItem('videoplayer-current-time');
+// console.log(timePlay);
+// const timeNumber = Number(timePlay);
+// console.log(timeNumber);
+// const parsTimePlay = JSON.parse(timePlay);
+// console.log(parsTimePlay);
+
+const onPlay = data => {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+  // console.log(currentTime);
+};
+
+// player.on('play', onPlay);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+const timePlay = localStorage.getItem('videoplayer-current-time');
+console.log('played the video!');
+
+player.getVideoTitle().then(function (title) {
+  // console.log('title:', title);
+});
+player
+  .setCurrentTime(timePlay)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
