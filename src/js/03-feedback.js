6697eb54b0299a51f962e2ea1bb5d10e60391b96
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-05-25 20:51:51 +0100

    add_form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  email: document.querySelector('.feedback-form input'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
+saveTextInput();
+refs.form.addEventListener('submit', onFormSubmit);
+
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+const formData = localStorage.getItem(STORAGE_KEY)
+  ? JSON.parse(localStorage.getItem(STORAGE_KEY))
+  : {};
+// const formData = {};
+
+saveTextInput();
+
+function onFormInput(e) {
+  formData[e.target.name] = e.target.value;
+
+  console.log(formData);
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function saveTextInput() {
+  const saveMessage = localStorage.getItem(STORAGE_KEY);
+  const parsMessage = JSON.parse(saveMessage);
+
+  if (parsMessage) {
+    refs.email.value = parsMessage.email;
+    refs.message.value = parsMessage.message;
+  }
+}
