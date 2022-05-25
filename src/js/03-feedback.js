e208123a264a785bc97d15836410a8cc3c02013c
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-05-25 21:03:48 +0100

    change_form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,7 +5,7 @@ const STORAGE_KEY = 'feedback-form-state';
 const refs = {
   form: document.querySelector('.feedback-form'),
   email: document.querySelector('.feedback-form input'),
-  textarea: document.querySelector('.feedback-form textarea'),
+  message: document.querySelector('.feedback-form textarea'),
 };
 saveTextInput();
 refs.form.addEventListener('submit', onFormSubmit);
@@ -22,8 +22,6 @@ saveTextInput();
 function onFormInput(e) {
   formData[e.target.name] = e.target.value;
 
-  console.log(formData);
-
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
@@ -31,14 +29,21 @@ function onFormSubmit(e) {
   e.preventDefault();
   e.target.reset();
   localStorage.removeItem(STORAGE_KEY);
+  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 }
 
 function saveTextInput() {
   const saveMessage = localStorage.getItem(STORAGE_KEY);
+
   const parsMessage = JSON.parse(saveMessage);
+  console.log(parsMessage);
 
   if (parsMessage) {
     refs.email.value = parsMessage.email;
+    // refs.textarea.value = parsMessage.textarea;
+  }
+  if (parsMessage) {
     refs.message.value = parsMessage.message;
+    // refs.textarea.value = parsMessage.textarea;
   }
 }
