2643ff6e6f94f965d8010033a1e2819a63a958fa
mankovskiy <35701366+mankovskiy@users.noreply.github.com>
2022-05-28 17:38:59 +0000

    Deploying to gh-pages from @ mankovskiy/goit-js-hw-08@ffcf1e9b5f30303cc439bf2dfb320b2d37fd716a 🚀

--- 01-gallery.66b22c3f.js
+++ 01-gallery.66b22c3f.js
@@ -8,4 +8,4 @@ var t=arguments[3];function e(t){return(e="function"==typeof Symbol&&"symbol"==t
 },{}],"TfQ5":[function(require,module,exports) {
 "use strict";var e=require("./gallery-items"),l=r(require("simplelightbox"));function r(e){return e&&e.__esModule?e:{default:e}}require("simplelightbox/dist/simple-lightbox.min.css"),console.log(e.galleryItems);const t=document.querySelector(".gallery"),a=i(e.galleryItems);function i(e){return e.map(({preview:e,original:l,description:r})=>`<a class="gallery__item" href="${l}">\n        <img class="gallery__image" src="${e}" alt="${r}" />\n      </a>`).join("")}t.insertAdjacentHTML("beforeend",a);let s=new l.default(".gallery a",{captionDelay:250,showCounter:!1,alertError:!1,captionsData:"alt"});
 },{"./gallery-items":"q5Rn","simplelightbox":"yx6k","simplelightbox/dist/simple-lightbox.min.css":"kz9M"}]},{},["TfQ5"], null)
-//# sourceMappingURL=/goit-js-hw-08/01-gallery.ca6c80ac.js.map
+//# sourceMappingURL=/goit-js-hw-08/01-gallery.66b22c3f.js.map
