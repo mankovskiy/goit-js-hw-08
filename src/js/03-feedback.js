24ebf93838623a3e6312b6dff82950a6e43b3205
Stanislav Mankovskyi <odnorazka25@gmail.com>
2022-05-29 11:55:50 +0100

    correct_form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -12,42 +12,37 @@ refs.form.addEventListener('submit', onFormSubmit);
 
 refs.form.addEventListener('input', throttle(onFormInput, 500));
 
-// const formData = {};
-let formData = localStorage.getItem(STORAGE_KEY)
+const formData = localStorage.getItem(STORAGE_KEY)
   ? JSON.parse(localStorage.getItem(STORAGE_KEY))
   : {};
 function onFormInput(e) {
-  formData[e.target.name] = e.target.value;
+  formData.email = e.currentTarget.email.value;
+  formData.message = e.currentTarget.message.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-saveTextInput();
-
 function onFormSubmit(e) {
   e.preventDefault();
-  // const formElements = e.currentTarget.elements;
   if (refs.input.value === '' || refs.textarea.value === '') {
     const message = 'Заполни все поля';
     return alert(message);
   }
   e.target.reset();
   localStorage.removeItem(STORAGE_KEY);
-  // console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
 }
 
-function saveTextInput() {
-  let saveMessage = localStorage.getItem(STORAGE_KEY);
+const saveTextInput = () => {
+  const saveMessage = JSON.parse(localStorage.getItem(STORAGE_KEY));
 
+  console.log(saveMessage);
   if (saveMessage) {
-    formData = JSON.parse(saveMessage);
-    // refs.input.value = saveMessage.email;
-    // refs.textarea.value = saveMessage.message;
-  }
-  if (formData.email) {
-    refs.input.value = formData.email;
-  }
+    if (formData.email) {
+      refs.input.value = formData.email;
+    }
 
-  if (formData.message) {
-    refs.textarea.value = formData.message;
+    if (formData.message) {
+      refs.textarea.value = formData.message;
+    }
   }
-}
+};
+saveTextInput();
